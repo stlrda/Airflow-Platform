a8e974d3825dcca4dd30a7b34666842d87ede6a8
Jonathan Leek <jonathanleek@gmail.com>
2019-12-13 00:06:42 -0600

    IAM Policy Assignments

---
+++ iam.tf
@@ -0,0 +1,37 @@
+#----------------------------------
+#Assigns IAM policies
+#----------------------------------
+
+resource "aws_iam_instance_profile" "airflow_profile" { #TODO does this serve all ec2 instances, or just the workers?
+  name = "${var.cluster_name}-profile"
+  path = "/instance-profile"
+}
+
+resource "aws_iam_role_policy_attachment" "s3_policy" {
+  role = "${var.cluster_name}-s3-policy"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "sqs_policy" {
+  role = "${var.cluster_name}-sqs-policy"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSQSFullAccess"
+}
+
+resource "aws_sqs_queue_policy" "sqs_permission" {
+  queue_url = aws_sqs_queue.airflow_queue.id
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "sqspolicy",
+  "Statement": [
+    {
+      "Sid": "First",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "SQS:*",
+      "Resource": "${aws_sqs_queue.airflow_queue.arn}"
+    }
+  ]
+}
+POLICY
+}
---
+++ sqs_queue.tf
@@ -0,0 +1,8 @@
+#----------------------------------
+#Creates an SQS queue # TODO Is this used to communicate with workers?
+#----------------------------------
+resource "aws_sqs_queue" "airflow_queue" {
+  name = "${var.cluster_name}-queue"
+  max_message_size = 262144
+  tags = var.tags
+}
