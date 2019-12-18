85198fe0524202f8c59a3fa6fba40f36f50c675b
Jonathan Leek <jonathanleek@gmail.com>
2019-12-18 14:05:51 -0600

    Bughunt

---
+++ main.tf
@@ -0,0 +1,7 @@
+//This file defines AWS as the cloud provider we will be using.
+//CHANGING THIS IS NOT SUFFICIENT TO SWITCH TO A DIFFERENT PROVIDER
+
+provider "aws" {
+  profile = "default"
+  region = var.aws_region
+}
