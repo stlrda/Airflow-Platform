30692d99245001cd47c2c3db0acf53d3b18f2606
Jonathan Leek <jonathanleek@gmail.com>
2020-01-15 10:36:11 -0600

    Remote Exec currently time out

--- ec2.tf
+++ ec2.tf
@@ -65,50 +65,50 @@ resource "aws_instance" "airflow_webserver" { #TODO need to setup start scripts 
     volume_size = var.root_volume_size
   }
 
-  provisioner "remote-exec" {
-    inline = [
-      "echo AIRFLOW__CORE__DEFAULT_TIMEZONE=America/Chicago | sudo tee -a /tmp/custom_env",
-      "echo psycopg2-binary | sudo tee -a /tmp/requirements.txt",
-      "echo AWS_DEFAULT_REGION=${var.aws_region} | sudo tee -a /tmp/airflow_environment",
-      "echo AIRFLOW_HOME=/usr/local/airflow | sudo tee -a /tmp/airflow_environment",
-      "echo AIRFLOW__CORE__EXECUTOR=CeleryExecutor | sudo tee -a /tmp/airflow_environment",
-      "echo AIRFLOW__CORE__FERNET_KEY=${var.fernet_key} | sudo tee -a /tmp/airflow_environment",
-      "echo AIRFLOW__CORE__LOAD_EXAMPLES=true | sudo tee -a /tmp/airflow_environment",
-      "echo AIRFLOW__CORE__LOAD_DEFAULTS=false | sudo tee -a /tmp/airflow_environment",
-      "echo AIRFLOW__CORE__SQL_ALCHEMY_CONN=postgresql+psycopg2://${var.db_username}:${var.db_password}@${aws_db_instance.airflow_database.endpoint}/${var.db_dbname} | sudo tee -a /tmp/airflow_environment",
-      "echo AIRFLOW__CORE__REMOTE_BASE_LOG_FOLDER=s3://${aws_s3_bucket.airflow_logs.id} | sudo tee -a /tmp/airflow_environment",
-      "echo AIRFLOW__CORE__REMOTE_LOGGING=True | sudo tee -a /tmp/airflow_environment",
-      "echo AIRFLOW__WEBSERVER__WEB_SERVER_PORT=8080 | sudo tee -a /tmp/airflow_environment",
-      "echo AIRFLOW__WEBSERVER__RBAC=True | sudo tee -a /tmp/airflow_environment",
-      "echo AIRFLOW__CELERY__BROKER_URL=sqs:// | sudo tee -a /tmp/airflow_environment",
-      "echo AIRFLOW__CELERY__DEFAULT_QUEUE=${var.cluster_name}-queue | sudo tee -a /tmp/airflow_environment",
-      "echo AIRFLOW__CELERY__RESULT_BACKEND=db+postgresql://${var.db_dbname}:${var.db_password}@${aws_db_instance.airflow_database.endpoint}/${var.db_dbname} | sudo tee -a /tmp/airflow_environment",
-      "echo AIRFLOW__CELERY__BROKER_TRANSPORT_OPTIONS__REGION=${var.aws_region} | sudo tee -a /tmp/airflow_environment",
-      "echo [Unit] | sudo tee -a /tmp/airflow.service",
-      "echo Description=Airflow daemon | sudo tee -a /tmp/airflow.service",
-      "echo After=network.target | sudo tee -a /tmp/airflow.service",
-      "echo [Service] | sudo tee -a /tmp/airflow.service",
-      "echo EnvironmentFile=/etc/sysconfig/airflow | sudo tee -a /tmp/airflow.service",
-      "echo User=ubuntu | sudo tee -a /tmp/airflow.service",
-      "echo Group=ubuntu | sudo tee -a /tmp/airflow.service",
-      "echo Type=simple | sudo tee -a /tmp/airflow.service",
-      "echo Restart=always | sudo tee -a /tmp/airflow.service",
-      "echo ExecStart=/usr/bin/terraform-aws-airflow | sudo tee -a /tmp/airflow.service",
-      "echo RestartSec=5s | sudo tee -a /tmp/airflow.service",
-      "echo PrivateTmp=true | sudo tee -a /tmp/airflow.service",
-      "echo [Install] | sudo tee -a /tmp/airflow.service",
-      "echo WantedBy=multi-user.target | sudo tee -a /tmp/airflow.service",
-      "echo AIRFLOW_ROLE=WEBSERVER | sudo tee -a /etc/environment",
-    ]
-
-    connection {
-      host        = self.public_ip
-      agent       = false
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = file(var.private_key_path)
-    }
-  }
+//  provisioner "remote-exec" {
+//    inline = [
+//      "echo AIRFLOW__CORE__DEFAULT_TIMEZONE=America/Chicago | sudo tee -a /tmp/custom_env",
+//      "echo psycopg2-binary | sudo tee -a /tmp/requirements.txt",
+//      "echo AWS_DEFAULT_REGION=${var.aws_region} | sudo tee -a /tmp/airflow_environment",
+//      "echo AIRFLOW_HOME=/usr/local/airflow | sudo tee -a /tmp/airflow_environment",
+//      "echo AIRFLOW__CORE__EXECUTOR=CeleryExecutor | sudo tee -a /tmp/airflow_environment",
+//      "echo AIRFLOW__CORE__FERNET_KEY=${var.fernet_key} | sudo tee -a /tmp/airflow_environment",
+//      "echo AIRFLOW__CORE__LOAD_EXAMPLES=true | sudo tee -a /tmp/airflow_environment",
+//      "echo AIRFLOW__CORE__LOAD_DEFAULTS=false | sudo tee -a /tmp/airflow_environment",
+//      "echo AIRFLOW__CORE__SQL_ALCHEMY_CONN=postgresql+psycopg2://${var.db_username}:${var.db_password}@${aws_db_instance.airflow_database.endpoint}/${var.db_dbname} | sudo tee -a /tmp/airflow_environment",
+//      "echo AIRFLOW__CORE__REMOTE_BASE_LOG_FOLDER=s3://${aws_s3_bucket.airflow_logs.id} | sudo tee -a /tmp/airflow_environment",
+//      "echo AIRFLOW__CORE__REMOTE_LOGGING=True | sudo tee -a /tmp/airflow_environment",
+//      "echo AIRFLOW__WEBSERVER__WEB_SERVER_PORT=8080 | sudo tee -a /tmp/airflow_environment",
+//      "echo AIRFLOW__WEBSERVER__RBAC=True | sudo tee -a /tmp/airflow_environment",
+//      "echo AIRFLOW__CELERY__BROKER_URL=sqs:// | sudo tee -a /tmp/airflow_environment",
+//      "echo AIRFLOW__CELERY__DEFAULT_QUEUE=${var.cluster_name}-queue | sudo tee -a /tmp/airflow_environment",
+//      "echo AIRFLOW__CELERY__RESULT_BACKEND=db+postgresql://${var.db_dbname}:${var.db_password}@${aws_db_instance.airflow_database.endpoint}/${var.db_dbname} | sudo tee -a /tmp/airflow_environment",
+//      "echo AIRFLOW__CELERY__BROKER_TRANSPORT_OPTIONS__REGION=${var.aws_region} | sudo tee -a /tmp/airflow_environment",
+//      "echo [Unit] | sudo tee -a /tmp/airflow.service",
+//      "echo Description=Airflow daemon | sudo tee -a /tmp/airflow.service",
+//      "echo After=network.target | sudo tee -a /tmp/airflow.service",
+//      "echo [Service] | sudo tee -a /tmp/airflow.service",
+//      "echo EnvironmentFile=/etc/sysconfig/airflow | sudo tee -a /tmp/airflow.service",
+//      "echo User=ubuntu | sudo tee -a /tmp/airflow.service",
+//      "echo Group=ubuntu | sudo tee -a /tmp/airflow.service",
+//      "echo Type=simple | sudo tee -a /tmp/airflow.service",
+//      "echo Restart=always | sudo tee -a /tmp/airflow.service",
+//      "echo ExecStart=/usr/bin/terraform-aws-airflow | sudo tee -a /tmp/airflow.service",
+//      "echo RestartSec=5s | sudo tee -a /tmp/airflow.service",
+//      "echo PrivateTmp=true | sudo tee -a /tmp/airflow.service",
+//      "echo [Install] | sudo tee -a /tmp/airflow.service",
+//      "echo WantedBy=multi-user.target | sudo tee -a /tmp/airflow.service",
+//      "echo AIRFLOW_ROLE=WEBSERVER | sudo tee -a /etc/environment",
+//    ]
+//
+//    connection {
+//      host        = self.public_ip
+//      agent       = false
+//      type        = "ssh"
+//      user        = "ubuntu"
+//      private_key = file(var.private_key_path)
+//    }
+//  }
 
   user_data= data.template_file.provisisioner.rendered
 
@@ -133,50 +133,50 @@ resource "aws_instance" "airflow_scheduler" { #TODO need to setup start scripts
     volume_size = var.root_volume_size
   }
 
-  provisioner "remote-exec" {
-    inline = [
-      "echo AIRFLOW__CORE__DEFAULT_TIMEZONE=America/Chicago | sudo tee -a /tmp/custom_env",
-      "echo psycopg2-binary | sudo tee -a /tmp/requirements.txt",
-      "echo AWS_DEFAULT_REGION=${var.aws_region} | sudo tee -a /tmp/airflow_environment",
-      "echo AIRFLOW_HOME=/usr/local/airflow | sudo tee -a /tmp/airflow_environment",
-      "echo AIRFLOW__CORE__EXECUTOR=CeleryExecutor | sudo tee -a /tmp/airflow_environment",
-      "echo AIRFLOW__CORE__FERNET_KEY=${var.fernet_key} | sudo tee -a /tmp/airflow_environment",
-      "echo AIRFLOW__CORE__LOAD_EXAMPLES=true | sudo tee -a /tmp/airflow_environment",
-      "echo AIRFLOW__CORE__LOAD_DEFAULTS=false | sudo tee -a /tmp/airflow_environment",
-      "echo AIRFLOW__CORE__SQL_ALCHEMY_CONN=postgresql+psycopg2://${var.db_username}:${var.db_password}@${aws_db_instance.airflow_database.endpoint}/${var.db_dbname} | sudo tee -a /tmp/airflow_environment",
-      "echo AIRFLOW__CORE__REMOTE_BASE_LOG_FOLDER=s3://${aws_s3_bucket.airflow_logs.id} | sudo tee -a /tmp/airflow_environment",
-      "echo AIRFLOW__CORE__REMOTE_LOGGING=True | sudo tee -a /tmp/airflow_environment",
-      "echo AIRFLOW__WEBSERVER__WEB_SERVER_PORT=8080 | sudo tee -a /tmp/airflow_environment",
-      "echo AIRFLOW__WEBSERVER__RBAC=True | sudo tee -a /tmp/airflow_environment",
-      "echo AIRFLOW__CELERY__BROKER_URL=sqs:// | sudo tee -a /tmp/airflow_environment",
-      "echo AIRFLOW__CELERY__DEFAULT_QUEUE=${var.cluster_name}-queue | sudo tee -a /tmp/airflow_environment",
-      "echo AIRFLOW__CELERY__RESULT_BACKEND=db+postgresql://${var.db_dbname}:${var.db_password}@${aws_db_instance.airflow_database.endpoint}/${var.db_dbname} | sudo tee -a /tmp/airflow_environment",
-      "echo AIRFLOW__CELERY__BROKER_TRANSPORT_OPTIONS__REGION=${var.aws_region} | sudo tee -a /tmp/airflow_environment",
-      "echo [Unit] | sudo tee -a /tmp/airflow.service",
-      "echo Description=Airflow daemon | sudo tee -a /tmp/airflow.service",
-      "echo After=network.target | sudo tee -a /tmp/airflow.service",
-      "echo [Service] | sudo tee -a /tmp/airflow.service",
-      "echo EnvironmentFile=/etc/sysconfig/airflow | sudo tee -a /tmp/airflow.service",
-      "echo User=ubuntu | sudo tee -a /tmp/airflow.service",
-      "echo Group=ubuntu | sudo tee -a /tmp/airflow.service",
-      "echo Type=simple | sudo tee -a /tmp/airflow.service",
-      "echo Restart=always | sudo tee -a /tmp/airflow.service",
-      "echo ExecStart=/usr/bin/terraform-aws-airflow | sudo tee -a /tmp/airflow.service",
-      "echo RestartSec=5s | sudo tee -a /tmp/airflow.service",
-      "echo PrivateTmp=true | sudo tee -a /tmp/airflow.service",
-      "echo [Install] | sudo tee -a /tmp/airflow.service",
-      "echo WantedBy=multi-user.target | sudo tee -a /tmp/airflow.service",
-      "echo AIRFLOW_ROLE=SCHEDULER | sudo tee -a /etc/environment",
-    ]
-
-    connection {
-      host        = self.public_ip
-      agent       = false
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = file(var.private_key_path)
-    }
-  }
+//  provisioner "remote-exec" {
+//    inline = [
+//      "echo AIRFLOW__CORE__DEFAULT_TIMEZONE=America/Chicago | sudo tee -a /tmp/custom_env",
+//      "echo psycopg2-binary | sudo tee -a /tmp/requirements.txt",
+//      "echo AWS_DEFAULT_REGION=${var.aws_region} | sudo tee -a /tmp/airflow_environment",
+//      "echo AIRFLOW_HOME=/usr/local/airflow | sudo tee -a /tmp/airflow_environment",
+//      "echo AIRFLOW__CORE__EXECUTOR=CeleryExecutor | sudo tee -a /tmp/airflow_environment",
+//      "echo AIRFLOW__CORE__FERNET_KEY=${var.fernet_key} | sudo tee -a /tmp/airflow_environment",
+//      "echo AIRFLOW__CORE__LOAD_EXAMPLES=true | sudo tee -a /tmp/airflow_environment",
+//      "echo AIRFLOW__CORE__LOAD_DEFAULTS=false | sudo tee -a /tmp/airflow_environment",
+//      "echo AIRFLOW__CORE__SQL_ALCHEMY_CONN=postgresql+psycopg2://${var.db_username}:${var.db_password}@${aws_db_instance.airflow_database.endpoint}/${var.db_dbname} | sudo tee -a /tmp/airflow_environment",
+//      "echo AIRFLOW__CORE__REMOTE_BASE_LOG_FOLDER=s3://${aws_s3_bucket.airflow_logs.id} | sudo tee -a /tmp/airflow_environment",
+//      "echo AIRFLOW__CORE__REMOTE_LOGGING=True | sudo tee -a /tmp/airflow_environment",
+//      "echo AIRFLOW__WEBSERVER__WEB_SERVER_PORT=8080 | sudo tee -a /tmp/airflow_environment",
+//      "echo AIRFLOW__WEBSERVER__RBAC=True | sudo tee -a /tmp/airflow_environment",
+//      "echo AIRFLOW__CELERY__BROKER_URL=sqs:// | sudo tee -a /tmp/airflow_environment",
+//      "echo AIRFLOW__CELERY__DEFAULT_QUEUE=${var.cluster_name}-queue | sudo tee -a /tmp/airflow_environment",
+//      "echo AIRFLOW__CELERY__RESULT_BACKEND=db+postgresql://${var.db_dbname}:${var.db_password}@${aws_db_instance.airflow_database.endpoint}/${var.db_dbname} | sudo tee -a /tmp/airflow_environment",
+//      "echo AIRFLOW__CELERY__BROKER_TRANSPORT_OPTIONS__REGION=${var.aws_region} | sudo tee -a /tmp/airflow_environment",
+//      "echo [Unit] | sudo tee -a /tmp/airflow.service",
+//      "echo Description=Airflow daemon | sudo tee -a /tmp/airflow.service",
+//      "echo After=network.target | sudo tee -a /tmp/airflow.service",
+//      "echo [Service] | sudo tee -a /tmp/airflow.service",
+//      "echo EnvironmentFile=/etc/sysconfig/airflow | sudo tee -a /tmp/airflow.service",
+//      "echo User=ubuntu | sudo tee -a /tmp/airflow.service",
+//      "echo Group=ubuntu | sudo tee -a /tmp/airflow.service",
+//      "echo Type=simple | sudo tee -a /tmp/airflow.service",
+//      "echo Restart=always | sudo tee -a /tmp/airflow.service",
+//      "echo ExecStart=/usr/bin/terraform-aws-airflow | sudo tee -a /tmp/airflow.service",
+//      "echo RestartSec=5s | sudo tee -a /tmp/airflow.service",
+//      "echo PrivateTmp=true | sudo tee -a /tmp/airflow.service",
+//      "echo [Install] | sudo tee -a /tmp/airflow.service",
+//      "echo WantedBy=multi-user.target | sudo tee -a /tmp/airflow.service",
+//      "echo AIRFLOW_ROLE=SCHEDULER | sudo tee -a /etc/environment",
+//    ]
+//
+//    connection {
+//      host        = self.public_ip
+//      agent       = false
+//      type        = "ssh"
+//      user        = "ubuntu"
+//      private_key = file(var.private_key_path)
+//    }
+//  }
 
   user_data= data.template_file.provisisioner.rendered
 
@@ -201,50 +201,50 @@ resource "aws_instance" "airflow_worker" { #TODO need to setup start scripts
     volume_size = var.root_volume_size
   }
 
-  provisioner "remote-exec" {
-    inline = [
-      "echo AIRFLOW__CORE__DEFAULT_TIMEZONE=America/Chicago | sudo tee -a /tmp/custom_env",
-      "echo psycopg2-binary | sudo tee -a /tmp/requirements.txt",
-      "echo AWS_DEFAULT_REGION=${var.aws_region} | sudo tee -a /tmp/airflow_environment",
-      "echo AIRFLOW_HOME=/usr/local/airflow | sudo tee -a /tmp/airflow_environment",
-      "echo AIRFLOW__CORE__EXECUTOR=CeleryExecutor | sudo tee -a /tmp/airflow_environment",
-      "echo AIRFLOW__CORE__FERNET_KEY=${var.fernet_key} | sudo tee -a /tmp/airflow_environment",
-      "echo AIRFLOW__CORE__LOAD_EXAMPLES=true | sudo tee -a /tmp/airflow_environment",
-      "echo AIRFLOW__CORE__LOAD_DEFAULTS=false | sudo tee -a /tmp/airflow_environment",
-      "echo AIRFLOW__CORE__SQL_ALCHEMY_CONN=postgresql+psycopg2://${var.db_username}:${var.db_password}@${aws_db_instance.airflow_database.endpoint}/${var.db_dbname} | sudo tee -a /tmp/airflow_environment",
-      "echo AIRFLOW__CORE__REMOTE_BASE_LOG_FOLDER=s3://${aws_s3_bucket.airflow_logs.id} | sudo tee -a /tmp/airflow_environment",
-      "echo AIRFLOW__CORE__REMOTE_LOGGING=True | sudo tee -a /tmp/airflow_environment",
-      "echo AIRFLOW__WEBSERVER__WEB_SERVER_PORT=8080 | sudo tee -a /tmp/airflow_environment",
-      "echo AIRFLOW__WEBSERVER__RBAC=True | sudo tee -a /tmp/airflow_environment",
-      "echo AIRFLOW__CELERY__BROKER_URL=sqs:// | sudo tee -a /tmp/airflow_environment",
-      "echo AIRFLOW__CELERY__DEFAULT_QUEUE=${var.cluster_name}-queue | sudo tee -a /tmp/airflow_environment",
-      "echo AIRFLOW__CELERY__RESULT_BACKEND=db+postgresql://${var.db_dbname}:${var.db_password}@${aws_db_instance.airflow_database.endpoint}/${var.db_dbname} | sudo tee -a /tmp/airflow_environment",
-      "echo AIRFLOW__CELERY__BROKER_TRANSPORT_OPTIONS__REGION=${var.aws_region} | sudo tee -a /tmp/airflow_environment",
-      "echo [Unit] | sudo tee -a /tmp/airflow.service",
-      "echo Description=Airflow daemon | sudo tee -a /tmp/airflow.service",
-      "echo After=network.target | sudo tee -a /tmp/airflow.service",
-      "echo [Service] | sudo tee -a /tmp/airflow.service",
-      "echo EnvironmentFile=/etc/sysconfig/airflow | sudo tee -a /tmp/airflow.service",
-      "echo User=ubuntu | sudo tee -a /tmp/airflow.service",
-      "echo Group=ubuntu | sudo tee -a /tmp/airflow.service",
-      "echo Type=simple | sudo tee -a /tmp/airflow.service",
-      "echo Restart=always | sudo tee -a /tmp/airflow.service",
-      "echo ExecStart=/usr/bin/terraform-aws-airflow | sudo tee -a /tmp/airflow.service",
-      "echo RestartSec=5s | sudo tee -a /tmp/airflow.service",
-      "echo PrivateTmp=true | sudo tee -a /tmp/airflow.service",
-      "echo [Install] | sudo tee -a /tmp/airflow.service",
-      "echo WantedBy=multi-user.target | sudo tee -a /tmp/airflow.service",
-      "echo AIRFLOW_ROLE=WORKER | sudo tee -a /etc/environment",
-    ]
-
-    connection {
-      host        = self.public_ip
-      agent       = false
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = file(var.private_key_path)
-    }
-  }
+//  provisioner "remote-exec" {
+//    inline = [
+//      "echo AIRFLOW__CORE__DEFAULT_TIMEZONE=America/Chicago | sudo tee -a /tmp/custom_env",
+//      "echo psycopg2-binary | sudo tee -a /tmp/requirements.txt",
+//      "echo AWS_DEFAULT_REGION=${var.aws_region} | sudo tee -a /tmp/airflow_environment",
+//      "echo AIRFLOW_HOME=/usr/local/airflow | sudo tee -a /tmp/airflow_environment",
+//      "echo AIRFLOW__CORE__EXECUTOR=CeleryExecutor | sudo tee -a /tmp/airflow_environment",
+//      "echo AIRFLOW__CORE__FERNET_KEY=${var.fernet_key} | sudo tee -a /tmp/airflow_environment",
+//      "echo AIRFLOW__CORE__LOAD_EXAMPLES=true | sudo tee -a /tmp/airflow_environment",
+//      "echo AIRFLOW__CORE__LOAD_DEFAULTS=false | sudo tee -a /tmp/airflow_environment",
+//      "echo AIRFLOW__CORE__SQL_ALCHEMY_CONN=postgresql+psycopg2://${var.db_username}:${var.db_password}@${aws_db_instance.airflow_database.endpoint}/${var.db_dbname} | sudo tee -a /tmp/airflow_environment",
+//      "echo AIRFLOW__CORE__REMOTE_BASE_LOG_FOLDER=s3://${aws_s3_bucket.airflow_logs.id} | sudo tee -a /tmp/airflow_environment",
+//      "echo AIRFLOW__CORE__REMOTE_LOGGING=True | sudo tee -a /tmp/airflow_environment",
+//      "echo AIRFLOW__WEBSERVER__WEB_SERVER_PORT=8080 | sudo tee -a /tmp/airflow_environment",
+//      "echo AIRFLOW__WEBSERVER__RBAC=True | sudo tee -a /tmp/airflow_environment",
+//      "echo AIRFLOW__CELERY__BROKER_URL=sqs:// | sudo tee -a /tmp/airflow_environment",
+//      "echo AIRFLOW__CELERY__DEFAULT_QUEUE=${var.cluster_name}-queue | sudo tee -a /tmp/airflow_environment",
+//      "echo AIRFLOW__CELERY__RESULT_BACKEND=db+postgresql://${var.db_dbname}:${var.db_password}@${aws_db_instance.airflow_database.endpoint}/${var.db_dbname} | sudo tee -a /tmp/airflow_environment",
+//      "echo AIRFLOW__CELERY__BROKER_TRANSPORT_OPTIONS__REGION=${var.aws_region} | sudo tee -a /tmp/airflow_environment",
+//      "echo [Unit] | sudo tee -a /tmp/airflow.service",
+//      "echo Description=Airflow daemon | sudo tee -a /tmp/airflow.service",
+//      "echo After=network.target | sudo tee -a /tmp/airflow.service",
+//      "echo [Service] | sudo tee -a /tmp/airflow.service",
+//      "echo EnvironmentFile=/etc/sysconfig/airflow | sudo tee -a /tmp/airflow.service",
+//      "echo User=ubuntu | sudo tee -a /tmp/airflow.service",
+//      "echo Group=ubuntu | sudo tee -a /tmp/airflow.service",
+//      "echo Type=simple | sudo tee -a /tmp/airflow.service",
+//      "echo Restart=always | sudo tee -a /tmp/airflow.service",
+//      "echo ExecStart=/usr/bin/terraform-aws-airflow | sudo tee -a /tmp/airflow.service",
+//      "echo RestartSec=5s | sudo tee -a /tmp/airflow.service",
+//      "echo PrivateTmp=true | sudo tee -a /tmp/airflow.service",
+//      "echo [Install] | sudo tee -a /tmp/airflow.service",
+//      "echo WantedBy=multi-user.target | sudo tee -a /tmp/airflow.service",
+//      "echo AIRFLOW_ROLE=WORKER | sudo tee -a /etc/environment",
+//    ]
+//
+//    connection {
+//      host        = self.public_ip
+//      agent       = false
+//      type        = "ssh"
+//      user        = "ubuntu"
+//      private_key = file(var.private_key_path)
+//    }
+//  }
 
   user_data= data.template_file.provisisioner.rendered
 
--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,21 @@
 resource "aws_vpc" "airflow_vpc" {
   cidr_block = "172.16.0.0/20"
   tags       = var.tags
+  enable_dns_hostnames = true
+}
+
+resource "aws_internet_gateway" "airflow_gateway" {
+  vpc_id = "${aws_vpc.airflow_vpc.id}"
+  tags = var.tags
+}
+
+resource "aws_route_table" "airflow_route_table" {
+  vpc_id = "${aws_vpc.airflow_vpc.id}"
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.airflow_gateway.id}"
+  }
+  tags = var.tags
 }
 
 resource "aws_subnet" "airflow_subnet_a" {
@@ -8,6 +23,7 @@ resource "aws_subnet" "airflow_subnet_a" {
   cidr_block = "172.16.0.0/27"
   tags       = var.tags
   availability_zone = "${var.aws_region}a"
+  map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "airflow_subnet_b" {
@@ -15,4 +31,5 @@ resource "aws_subnet" "airflow_subnet_b" {
   cidr_block = "172.16.0.32/27"
   tags       = var.tags
   availability_zone = "${var.aws_region}b"
+  map_public_ip_on_launch = true
 }
