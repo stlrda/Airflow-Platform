5ce8c59ffc22247127a80cb89d4353afc238a8f7
Wes Bradley <wbradley.work@gmail.com>
2020-01-15 19:02:40 -0600

    add subnet route table associations

--- vpc.tf
+++ vpc.tf
@@ -5,21 +5,21 @@ resource "aws_vpc" "airflow_vpc" {
 }
 
 resource "aws_internet_gateway" "airflow_gateway" {
-  vpc_id = "${aws_vpc.airflow_vpc.id}"
+  vpc_id = aws_vpc.airflow_vpc.id
   tags = var.tags
 }
 
 resource "aws_route_table" "airflow_route_table" {
-  vpc_id = "${aws_vpc.airflow_vpc.id}"
+  vpc_id = aws_vpc.airflow_vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.airflow_gateway.id}"
+    gateway_id = aws_internet_gateway.airflow_gateway.id
   }
   tags = var.tags
 }
 
 resource "aws_subnet" "airflow_subnet_a" {
-  vpc_id     = "${aws_vpc.airflow_vpc.id}"
+  vpc_id     = aws_vpc.airflow_vpc.id
   cidr_block = "172.16.0.0/27"
   tags       = var.tags
   availability_zone = "${var.aws_region}a"
@@ -27,9 +27,19 @@ resource "aws_subnet" "airflow_subnet_a" {
 }
 
 resource "aws_subnet" "airflow_subnet_b" {
-  vpc_id     = "${aws_vpc.airflow_vpc.id}"
+  vpc_id     = aws_vpc.airflow_vpc.id
   cidr_block = "172.16.0.32/27"
   tags       = var.tags
   availability_zone = "${var.aws_region}b"
   map_public_ip_on_launch = true
+}
+
+resource "aws_route_table_association" "airflow_subnet_a_route_table" {
+  subnet_id      = aws_subnet.airflow_subnet_a.id
+  route_table_id = aws_route_table.airflow_route_table.id
+}
+
+resource "aws_route_table_association" "airflow_subnet_b_route_table" {
+  subnet_id      = aws_subnet.airflow_subnet_b.id
+  route_table_id = aws_route_table.airflow_route_table.id
 }
