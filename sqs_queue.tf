a279c44c437af0843f03f5567f59fdcffa1e23a1
Jonathan Leek <jonathanleek@gmail.com>
2020-02-28 19:19:52 -0600

    Updated tfvars.example, disabled airflow.cfg

--- sqs_queue.tf
+++ sqs_queue.tf
@@ -6,3 +6,16 @@ resource "aws_sqs_queue" "airflow_queue" {
   max_message_size = 262144
   tags             = var.tags
 }
+
+
+#----------------------------------
+#Creates a Redis queue
+#----------------------------------
+resource "aws_elasticache_cluster" "airflow_queue" {
+  name             = "${var.cluster_name}-queue"
+  max_message_size = 262144
+  tags             = var.tags
+  cluster_id = ""
+}
+
+
