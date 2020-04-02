05899d890f41ffea2886b5c52508a0d638a77e57
Jonathan Leek <jonathanleek@gmail.com>
2020-04-02 13:57:00 -0500

    Started outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,14 @@
 output "login_url" {
   description = "Login page for the cluster"
   value = "${aws_instance.airflow_webserver.public_ip}:8080"
+}
 
+output "cluster_vpc_id"{
+  description = "AWS VPC the cluster resides in"
+  value = aws_vpc.airflow_vpc.id
+}
+
+output "cluster_nodes_sg_id" {
+  description = "AWS Security group id for the cluster nodes"
+  value = aws_security_group.ec2-sg.id
 }
