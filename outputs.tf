453c2e9c3bb38f76c6d1ef58eba6659d0eaea381
Jonathan Leek <jonathanleek@gmail.com>
2020-04-13 09:25:56 -0500

    Creating db_subnet_group output, don't need cluster_nodes_subnet_group

--- outputs.tf
+++ outputs.tf
@@ -13,7 +13,7 @@ output "cluster_nodes_sg_id" {
   value = aws_security_group.ec2-sg.id
 }
 
-output "cluster_nodes_subnet_group" {
-  description = "AWS subnet group ec2 nodes reside in"
-  value = aws_subnet.airflow_subnet_a.id
+output "db_subnet_group" {
+  description = "AWS subnet group db resides in"
+  value = aws_db_subnet_group.rds_subnet_group.id
 }
